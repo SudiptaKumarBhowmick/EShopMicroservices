cdc0116c2d479831c7f765bd9d06f0dbfccb4e10
Sudipta Bhowmick <sudipta.bhowmick@valmetpartners.com>
2025-03-20 17:59:37 +0600

    Implemented Get Product feature end point and command handler

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -16,7 +16,7 @@
 
                 var response = result.Adapt<CreateProductResponse>();
 
-                return Results.Created($"/products/{response.Id}", result.Id);
+                return Results.Created($"/products/{response.Id}", response);
             })
             .WithName("CreateProduct")
             .Produces<CreateProductResponse>(StatusCodes.Status201Created)
