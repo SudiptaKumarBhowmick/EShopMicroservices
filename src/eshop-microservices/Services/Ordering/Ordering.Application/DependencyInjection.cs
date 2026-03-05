d79f7563ce7651f6a8de9e2e030667dc6ceb7ca6
SudiptaKumarBhowmick <sudiptakumar.shuvo@gmail.com>
2026-03-05 22:12:50 +0600

    Implemented Dispatch Domain Event Interceptor and updated order db credentials

--- src/eshop-microservices/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/eshop-microservices/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -9,12 +9,12 @@ namespace Ordering.Application
         public static IServiceCollection AddApplicationServices
             (this IServiceCollection services, IConfiguration configuration)
         {
-            //services.AddMediatR(config =>
-            //{
-            //    config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-            //    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-            //    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-            //});
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                //config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                //config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            });
 
             //services.AddFeatureManagement();
             //services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
--- src/eshop-microservices/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/eshop-microservices/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Ordering.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace Ordering.Infrastructure
 {
@@ -13,12 +14,12 @@ namespace Ordering.Infrastructure
             var connectionString = configuration.GetConnectionString("Database");
 
             // Add services to the container.
-            //services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
-            //services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
 
             services.AddDbContext<ApplicationDbContext>((sp, options) =>
             {
-                options.AddInterceptors(new AuditableEntityInterceptor());
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
                 options.UseSqlServer(connectionString);
             });
 
